85c8cfd26a0d23cfd8411915497267d64ad3636d
a0988116128-bit <a0988116128@gmail.com>
2026-09-20 18:24:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.secret_key = "pezang_fixed_duplicate_endpoint_2026"
 
 DATABASE_URL = os.environ.get(
     "DATABASE_URL", 
-    "postgresql://postgres.gutyrssxtpuxndflkceq:你的真實密碼@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres"
+    "postgresql://postgres.gutyrssxtpuxndflkceq:Erin83390454@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres"
 )
 
 def get_db_connection():
